79f74c0d13d330ba67688a51c55ee97f4c1552c6
mirian2550 <miri2550@gmail.com>
2023-06-16 00:10:08 -0300

    descarga de archivo json

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+
+def descargar_archivo(url, ruta_guardado):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(ruta_guardado, 'wb') as archivo:
+            archivo.write(response.content)
+        print("Archivo descargado correctamente.")
+    else:
+        print("Error al descargar el archivo.")
+
+# URL del archivo JSON
+url = "https://raw.githubusercontent.com/benoitvallon/100-best-books/master/books.json"
+
+# Ruta donde deseas guardar el archivo
+ruta_guardado = "archivo_redes/books.json"
+
+# Llamada a la función para descargar el archivo
+descargar_archivo(url, ruta_guardado)
