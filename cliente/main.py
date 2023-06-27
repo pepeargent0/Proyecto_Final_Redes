2d48f6397b4ffcb52794af0c20b067029fe01bd2
pepe <dani_rc_22@hotmail.com>
2023-06-27 17:09:46 -0300

    cambios cliente

--- cliente/main.py
+++ cliente/main.py
@@ -1,111 +1,116 @@
 import requests
 
-# URL base del servidor FastAPI
+# URL base del servidor
 base_url = "http://localhost:8000"
 
+def get_books(params=None):
+    """
+    Obtiene la lista de libros desde el servidor.
 
-# Obtener información de la versión de la API
-def get_api_version():
-    url = f"{base_url}/"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        version = data.get("version")
-        print(f"Versión de la API: {version}")
-    else:
-        print("Error al obtener la versión de la API")
+    Parámetros:
+    - params (dict): Parámetros de filtrado de libros (opcional).
 
-
-# Obtener la lista de libros filtrada
-def get_filtered_books(params):
+    Retorna:
+    - data (dict): Datos de respuesta del servidor.
+    """
     url = f"{base_url}/books"
     response = requests.get(url, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        books = data.get("books")
-        count = data.get("count")
-        print(f"Total de libros encontrados: {count}")
-        for book in books:
-            print("-----")
-            print(f"Autor: {book['author']}")
-            print(f"Título: {book['title']}")
-            # Agrega más campos según tus necesidades
-    else:
-        print("Error al obtener la lista de libros")
+    response.raise_for_status()  # Verificar si se produjo un error en la respuesta
+    data = response.json()
+    return data
 
-
-# Crear un nuevo libro
 def create_book(book_data):
+    """
+    Crea un nuevo libro en el servidor.
+
+    Parámetros:
+    - book_data (dict): Datos del libro a crear.
+
+    Retorna:
+    - data (dict): Datos de respuesta del servidor.
+    """
     url = f"{base_url}/books"
     response = requests.post(url, json=book_data)
-    if response.status_code == 201:
-        data = response.json()
-        book = data.get("books")
-        print("Libro creado exitosamente:")
-        print(f"Autor: {book['author']}")
-        print(f"Título: {book['title']}")
-        # Agrega más campos según tus necesidades
-    else:
-        print("Error al crear el libro")
+    response.raise_for_status()  # Verificar si se produjo un error en la respuesta
+    data = response.json()
+    return data
 
+def delete_books(params=None):
+    """
+    Elimina libros del servidor según los parámetros proporcionados.
 
-# Eliminar libros filtrados
-def delete_filtered_books(params):
+    Parámetros (opcionales):
+    - params (dict): Parámetros de filtrado de libros a eliminar.
+
+    Retorna:
+    - data (dict): Datos de respuesta del servidor.
+    """
     url = f"{base_url}/books"
     response = requests.delete(url, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        count = data.get("count")
-        print(f"Total de libros eliminados: {count}")
-    else:
-        print("Error al eliminar los libros")
+    response.raise_for_status()  # Verificar si se produjo un error en la respuesta
+    data = response.json()
+    return data
 
-
-# Actualizar un libro por título
 def update_book_by_title(title, book_data):
+    """
+    Actualiza un libro en el servidor según el título.
+
+    Parámetros:
+    - title (str): Título del libro a actualizar.
+    - book_data (dict): Datos actualizados del libro.
+    Retorna:
+    - data (dict): Datos de respuesta del servidor.
+    """
     url = f"{base_url}/books/{title}"
     response = requests.put(url, json=book_data)
-    if response.status_code == 200:
-        data = response.json()
-        books = data.get("books")
-        count = data.get("count")
-        print(f"Total de libros actualizados: {count}")
-        for book in books:
-            print("-----")
-            print(f"Autor: {book['author']}")
-            print(f"Título: {book['title']}")
-            # Agrega más campos según tus necesidades
-    else:
-        print("Error al actualizar el libro")
-
-
-# Obtener información de la versión de la API
-get_api_version()
-
-# Filtrar libros por autor y año
-params = {
-    "author": "Autor Ejemplo",
-    "year": 2022
-}
-get_filtered_books(params)
-
-# Crear un nuevo libro
-new_book = {
-    "author": "Nuevo Autor",
-    "title": "Nuevo Libro",
-    # Agrega más campos según los definidos en el modelo Book
-}
-create_book(new_book)
-
-# Eliminar libros por país
-params = {
-    "country": "País Ejemplo"
-}
-delete_filtered_books(params)
-
-# Actualizar un libro por título
-title = "Título Ejemplo"
-updated_book = {
-    "author": "Autor Actualizado"
-}
-update_book_by_title(title, updated_book)
+    response.raise_for_status()  # Verificar si se produjo un error en la respuesta
+    data = response.json()
+    return data
+# Ejemplos de uso
+try:
+    # Obtener la lista de todos los libros
+    books_data = get_books()
+    print("Lista de libros:")
+    for book in books_data['books']:
+        print(book)
+    # Filtrar libros por autor
+    params = {'author': 'John Doe'}
+    filtered_books_data = get_books(params)
+    print("Lista de libros filtrados por autor:")
+    for book in filtered_books_data['books']:
+        print(book)
+    # Crear un nuevo libro
+    new_book_data = {
+        "author": "Jane Smith",
+        "country": "United States",
+        "imageLink": "example.com/image.jpg",
+        "language": "English",
+        "link": "example.com/book",
+        "pages": 200,
+        "title": "New Book",
+        "year": 2022
+    }
+    created_book_data = create_book(new_book_data)
+    print("Libro creado:")
+    print(created_book_data['books'])
+    # Eliminar libros por año
+    delete_params = {'year': 2020}
+    deleted_books_data = delete_books(delete_params)
+    print(deleted_books_data)
+    print(f"Libros eliminados: {deleted_books_data['count']}")
+    # Actualizar un libro por título
+    update_book_data = {
+        "author": "Jane Smith",
+        "year": 2023
+    }
+    updated_book_data = update_book_by_title("New Book", update_book_data)
+    print("Libro actualizado:")
+    print(updated_book_data['books'])
+except requests.exceptions.HTTPError as e:
+    print(f"Error en la solicitud HTTP: {e}")
+except requests.exceptions.RequestException as e:
+    print(f"Error al realizar la solicitud: {e}")
+except KeyError as e:
+    print(f"Error de clave: {e}")
+except Exception as e:
+    print(f"Error inesperado: {e}")
